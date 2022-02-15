820bf95c1a31d54a164045bdf6a42262138616e1
EgorKunickiy <91185203+EgorKunickiy@users.noreply.github.com>
2022-02-16 00:14:22 +0300

    .

--- Количества дней.py
+++ Количества дней.py
@@ -1,32 +1,23 @@
-from enum import Enum
 import math
 
-class Week (Enum):
-    Monday = ('понедельник', 1)
-    Tuesday = ('вторник', 2)
-    Wednesday = ('среда', 3)
-    Thursday = ('четверг', 4)
-    Friday = ('пятница', 5)
-    Saturday = ('суббота', 6)
-    Sunday = ('воскресенье', 7)
-
-    def __init__(self, day, num):
-        self.day = day
-        self.num = num
-
 def calculate_day (weekday):
-    list_day = [Week.Monday, Week.Tuesday, Week.Wednesday, Week.Thursday, Week.Friday, Week.Saturday, Week.Sunday]
     weekday = weekday.lower()
+    list_days = dict([
+                    ('понедельник', 1),
+                    ('вторник', 2),
+                    ('среда', 3),
+                    ('четверг', 4),
+                    ('пятница', 5),
+                    ('суббота', 6),
+                    ('воскресенье', 7)])
+    if weekday == 'суббота':
+        return list_days['воскресенье'] - list_days['суббота']
 
-    if weekday == Week.Saturday.value[0]:
-        return Week.Sunday.value[1] - Week.Saturday.value[1]
-
-    elif weekday == Week.Sunday.value[0]:
+    elif weekday == 'воскресенье':
         return 6
 
-    for day in list_day:
-        if day.value[0] == weekday:
-            return Week.Saturday.value[1] - day.value[1]
+    else:
+        return list_days['суббота'] - list_days[weekday]
 
 if __name__ == '__main__':
     print(calculate_day('суббота'))
