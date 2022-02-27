0acc65ba599f128e987ce6a980a136747a0e85ce
EgorKunickiy <91185203+EgorKunickiy@users.noreply.github.com>
2022-02-27 20:58:47 +0300

    .

--- parameter_validation.py
+++ parameter_validation.py
@@ -1,7 +1,14 @@
+def validate_arguments(function_to_validation):
+    def a_wrapper_accepting_arguments(a: str, b: int, c: dict):
+        assert isinstance(a, str), "a не является строкой"
+        assert isinstance(b, int), "b не является обьектом целочисленного типа"
+        assert isinstance(c, dict), "c не является словарем"
+        function_to_validation(a, b, c)
+    return a_wrapper_accepting_arguments
+
+@validate_arguments
 def func(a: str, b: int, c: dict):
-    assert isinstance(a, str), "a не является строкой"
-    assert isinstance(b, int), "b не является обьектом целочисленного типа"
-    assert isinstance(c, dict), "c не является словарем"
+    print('ok')
 
 
 if __name__ == "__main__":
