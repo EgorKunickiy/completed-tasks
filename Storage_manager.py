351c790a64c91ac84cda52418ffd22fdb71ea244
EgorKunickiy <91185203+EgorKunickiy@users.noreply.github.com>
2022-03-01 01:50:49 +0300

    .

---
+++ Storage_manager.py
@@ -0,0 +1,40 @@
+class Storage:
+    def __init__(self):
+        self.__is_appendable = False
+        self.__data = []
+
+    def show(self):
+        return self.__data
+
+    def append(self, odj):
+        if self.__is_appendable:
+            self.__data.append(odj)
+        else:
+            print('storage close')
+
+    def lock(self):
+        self.__is_appendable = False
+
+    def unlock_(self):
+        self.__is_appendable = True
+
+    def __enter__(self):
+        if not self.__is_appendable:
+            self.unlock_()
+            print('open')
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.lock()
+        print('close')
+
+
+if __name__ == "__main__":
+    storage = Storage()
+    storage.append(12345)
+
+    with Storage() as st:
+        st.append('abc')
+        print(st.show())
+        st.append({3: 5, 6: 8})
+        print(st.show())
