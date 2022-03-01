b224e6a8d26036708b22f831897807fcf0dbb174
EgorKunickiy <91185203+EgorKunickiy@users.noreply.github.com>
2022-03-01 22:30:45 +0300

    .

--- execution_time.py
+++ execution_time.py
@@ -2,25 +2,26 @@ from time import perf_counter
 
 
 class FindTime:
-    def __init__(self, state: bool):
-        self.state = state
+    def __init__(self):
+        self.time = 0
+
+    def get_time(self):
+        return self.time
 
     def __enter__(self):
         self.start = perf_counter()
         self.end = 0.0
-        if not self.state:
-            return lambda: self.end - self.start
-        else:
-            return self
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.end = perf_counter()
-        if self.state:
-            print(self.end - self.start)
+        self.time = self.end - self.start
 
 
 if __name__ == "__main__":
-    with FindTime(True) as timer:
+    with FindTime() as timer:
         a = 1
         for i in range(10000):
             a += 1
+
+    print(timer.get_time())
