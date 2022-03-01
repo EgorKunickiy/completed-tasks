cf3e09797c6436b1e4fe14390b0f91342e861723
EgorKunickiy <91185203+EgorKunickiy@users.noreply.github.com>
2022-03-01 23:49:38 +0300

    .

--- parameter_validation.py
+++ parameter_validation.py
@@ -1,16 +1,33 @@
+import inspect
+
+
 def validate_arguments(function_to_validation):
-    def a_wrapper_accepting_arguments(a: str, b: int, c: dict):
-        assert isinstance(a, str), "a не является строкой"
-        assert isinstance(b, int), "b не является обьектом целочисленного типа"
-        assert isinstance(c, dict), "c не является словарем"
-        function_to_validation(a, b, c)
-    return a_wrapper_accepting_arguments
+    sig_func = inspect.signature(function_to_validation)
+    names_args = list(sig_func.parameters.keys())
+
+    def function_to_receive_arguments(*args):
+        if len(names_args) == 1:
+            for arg in args:
+                assert (isinstance(arg, sig_func.parameters[names_args[0]].annotation)), 'does not match the data type'
+        else:
+            for arg, name_arg in zip(args, names_args):
+                assert (isinstance(arg, sig_func.parameters[name_arg].annotation)), 'does not match the data type'
+
+        function_to_validation(*args)
+    return function_to_receive_arguments
+
 
 @validate_arguments
 def func(a: str, b: int, c: dict):
     print('ok')
 
 
+@validate_arguments
+def func2(*args: int):
+    print('ok')
+
+
 if __name__ == "__main__":
-    func('qwer', 4, {2: 4})
-    func(2, 3, {2: 4})
+    func2(1, 2, 3, 445)
+    func('qwer', [6, 7, 8], {2: 4})
+
