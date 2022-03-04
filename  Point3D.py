59caeee89ccd590c659939190099cdabc6693a11
EgorKunickiy <91185203+EgorKunickiy@users.noreply.github.com>
2022-03-04 13:26:49 +0300

    new solution

---
+++  Point3D.py
@@ -0,0 +1,30 @@
+from math import pow, sqrt
+
+
+class Point3D:
+    def __init__(self, x: float, y: float, z: float):
+        self.__x = x
+        self.__y = y
+        self.__z = z
+
+    @property
+    def get_x(self):
+        return self.__x
+
+    @property
+    def get_y(self):
+        return self.__y
+
+    @property
+    def get_z(self):
+        return self.__z
+
+    def find_the_distance(self, point):
+        return sqrt(pow(self.__x - point.get_x, 2)+pow(self.__y - point.get_y, 2)+pow(self.__z - point.get_z, 2))
+
+
+if __name__ == "__main__":
+    point_1 = Point3D(1, 2, 3)
+    print(point_1.get_x)
+    point_2 = Point3D(3, 4, 6)
+    print(point_1.find_the_distance(point_2))
---
+++ ordering.py
@@ -0,0 +1,105 @@
+class ExceptionOfOrder(Exception):
+    pass
+
+
+class Dish:
+    def __init__(self, quantity: int, name: str, price: float, mass: int):
+        self.__quantity = quantity
+        self.__name = name
+        self.__price = price
+        self.__mass = mass
+
+    @property
+    def get_quantity(self):
+        return self.__quantity
+
+    @property
+    def get_name(self):
+        return self.__name
+
+    @property
+    def get_price(self):
+        return self.__price
+
+    @property
+    def get_mass(self):
+        return self.__mass
+
+
+class Ordering:
+    def __init__(self):
+        self.__list_of_dishes = None
+        self.__quantity_of_dish = 0
+        self.__price = 0
+        self.__mass = 0
+        self.__cost = 0
+    @property
+    def get_quantity(self):
+        return self.__quantity_of_dish
+
+    @property
+    def get_list_of_dish(self) -> list:
+        return self.__list_of_dishes
+
+    @property
+    def get_price(self):
+        return self.__price
+
+    @property
+    def get_mass(self):
+        return self.__mass
+
+    @property
+    def get_cost(self):
+        return self.__cost
+
+    def describe_the_order(self):
+        try:
+            if self.__list_of_dishes is None:
+                raise ExceptionOfOrder
+            else:
+                print(f'list of dish: {list(map(lambda x: x.get_name,self.get_list_of_dish))}', sep=', ')
+                print(f'total quantity: {self.get_quantity}' + '\n'
+                      f'total mass: {self.get_mass}' + '\n'
+                      f'total price: {self.get_price}' + '\n'
+                      f'total cost: {self.get_cost}')
+
+        except ExceptionOfOrder:
+            print(f'Order is empty')
+
+    def append_dish(self, *args: Dish):
+        self.__list_of_dishes = [dish for dish in args]
+        self.__recalculate_data()
+
+    def __recalculate_data(self):
+        for dish in self.__list_of_dishes:
+            self.__quantity_of_dish += dish.get_quantity
+            self.__mass += dish.get_mass
+            self.__price += dish.get_price * dish.get_quantity
+        self.__cost = self.__price
+
+    def pay(self, value):
+        self.__cost -= value
+        if self.__cost == 0:
+            print('order paid')
+        elif self.__cost < 0:
+            print(f'order paid, change: {abs(self.__cost)}')
+            self.__cost = 0
+        else:
+            print(f'left to pay {self.get_cost}')
+
+
+if __name__ == "__main__":
+    dish_1 = Dish(1, 'croissant', 1.55, 130)
+    dish_2 = Dish(1, 'soup', 4, 300)
+    dish_3 = Dish(2, 'tea', 2.05, 100)
+
+    ordering = Ordering()
+    ordering.append_dish(dish_1, dish_2, dish_3)
+    ordering.describe_the_order()
+    ordering.pay(10)
+
+
+
+    ordering_2 = Ordering()
+    ordering_2.describe_the_order()
