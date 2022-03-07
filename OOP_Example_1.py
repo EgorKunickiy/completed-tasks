486800d33472f152c9e4a88ff76adeb62e977eaa
EgorKunickiy <91185203+EgorKunickiy@users.noreply.github.com>
2022-03-08 00:36:04 +0300

    new solution

--- OOP_Example_1.py
+++ OOP_Example_1.py
@@ -1,14 +1,14 @@
-"""Answer to the question: inheritance, polymorphism, encapsulation"""
+"""Answer to the question: inheritance, association, encapsulation"""
 
 
 class Student:
-    def __init__(self, name):
-        self.__name = name
+    def __init__(self, name: str):
+        self.__name: str = name
 
 
 class EducationalInstitution:
-    def __init__(self, list_st):
-        self.__list_of_students = list_st
+    def __init__(self, list_st: list):
+        self.__list_of_students: list = list_st
 
     @property
     def list_of_students(self):
@@ -20,11 +20,19 @@ class EducationalInstitution:
 
 
 class School(EducationalInstitution):
-    def __init__(self, list_student):
+    def __init__(self, list_student: list):
         super().__init__(list_student)
 
 
 class University(EducationalInstitution):
-    def __init__(self, list_student):
+    def __init__(self, list_student: list):
         super().__init__(list_student)
 
+
+if __name__ == '__main__':
+    student1 = Student('Anna')
+    student2 = Student('Dima')
+    student3 = Student('Egor')
+    list_of_students = [student1, student2, student3]
+    school = School(list_of_students)
+    university = University(list_of_students)
