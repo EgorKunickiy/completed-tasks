b5ad1c2f3567156139792d465089d7356a103d96
EgorKunickiy <91185203+EgorKunickiy@users.noreply.github.com>
2022-03-14 22:23:41 +0300

    new solution

--- Mathematical_socket_server.py
+++ Mathematical_socket_server.py
@@ -1,5 +1,6 @@
 import socket
 import operator
+from mathematical_logic import Logic
 
 
 class ValidationException(Exception):
@@ -20,23 +21,10 @@ class Server:
                 while True:
                     data = conn.recv(1024).decode('UTF-8')
                     print(data)
-                    query = self.__multi_func(data)
+                    query = Logic.multi_func(data)
                     if not data:
                         break
                     conn.sendall(bytes(str(query), encoding="UTF-8"))
-
-    @staticmethod
-    def __multi_func(data: str):
-        try:
-            name_operator, num1, num2 = data.split(', ')
-            num1 = float(num1)
-            num2 = float(num2)
-            result = getattr(operator, name_operator)(num1, num2)
-            return result
-        except AttributeError:
-            return ' '
-        except ValueError:
-            return ' '
 
 
 if __name__ == "__main__":
---
+++ mathematical_logic.py
@@ -0,0 +1,16 @@
+import operator
+
+
+class Logic:
+    @staticmethod
+    def multi_func(data: str):
+        try:
+            name_operator, num1, num2 = data.split(', ')
+            num1 = float(num1)
+            num2 = float(num2)
+            result = getattr(operator, name_operator)(num1, num2)
+            return result
+        except AttributeError:
+            return ' '
+        except ValueError:
+            return ' '
