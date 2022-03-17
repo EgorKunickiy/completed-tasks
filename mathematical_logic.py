efdcde60c764f02c5e0ced46dc46edb9260a2e44
EgorKunickiy <91185203+EgorKunickiy@users.noreply.github.com>
2022-03-17 13:48:13 +0300

    new solution

--- mathematical_logic.py
+++ mathematical_logic.py
@@ -1,12 +1,30 @@
 import operator
 
+LIST_OF_OPERATOR = {
+    'add': operator.add,
+    'floordiv': operator.floordiv,
+    'mod': operator.mod,
+    'mul': operator.mul,
+    'lt': operator.lt,
+    'le': operator.le,
+    'eq': operator.eq,
+    'ne': operator.ne,
+    'ge': operator.ge,
+    'gt': operator.gt,
+    'pow': operator.pow,
+    'sub': operator.sub,
+    'truediv': operator.truediv
+}
+
 
 def multi_func(data: str) -> str:
     try:
         name_operator, num1, num2 = data.split(' ')
+        if name_operator not in LIST_OF_OPERATOR.keys():
+            raise AttributeError
         num1 = float(num1)
         num2 = float(num2)
-        result = getattr(operator, name_operator)(num1, num2)
+        result = LIST_OF_OPERATOR[name_operator](num1, num2)
         return str(result)
     except AttributeError:
         return ' '
