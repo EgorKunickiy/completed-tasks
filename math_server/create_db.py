4145012a5838ca68ea6769f8b10d0fbe052a9f85
EgorKunickiy <91185203+EgorKunickiy@users.noreply.github.com>
2022-03-23 00:38:26 +0300

    new solution

--- math_server/create_db.py
+++ math_server/create_db.py
@@ -4,26 +4,11 @@ from sqlalchemy import Column, Integer, Numeric, Enum
 import enum
 
 
-class Operators(enum.Enum):
-    add = 'add',
-    floordiv = 'floordiv',
-    mod = 'mod',
-    mul = 'mul',
-    lt = 'lt',
-    le = 'le',
-    eq = 'eq',
-    ne = 'ne',
-    ge = 'ge',
-    gt = 'gt',
-    pow = 'pow',
-    sub = 'sub',
-    truediv = 'truediv'
-
-
 class MathExpression(Base):
     __tablename__ = 'math_expression'
     id = Column(Integer, primary_key=True)
-    operator_ex = Column('operator', Enum(Operators, name='operators', create_type=False))
+    operator_ex = Column('operator', Enum(enum.Enum('Operator', mathematical_logic.LIST_OF_OPERATOR),
+                                          name='operators', create_type=False))
     number1 = Column('number1', Numeric)
     number2 = Column('number2', Numeric)
     result = Column('result', Numeric)
