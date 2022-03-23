a1bf2d44bf1660af939c86ea42f0e809a9d5abd8
EgorKunickiy <91185203+EgorKunickiy@users.noreply.github.com>
2022-03-23 03:39:58 +0300

    new solution

--- math_server/database_for_server.py
+++ math_server/database_for_server.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from data_for_db import user, password, database, host, port
+from math_server.data_for_db import user, password, database, host, port
 
 
 engine = create_engine(f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}')
