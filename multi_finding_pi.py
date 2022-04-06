0b548a2a648dbef5814ed1e9ea1c2c5e4d29937b
EgorKunickiy <91185203+EgorKunickiy@users.noreply.github.com>
2022-04-06 13:45:26 +0300

    new solution

--- multi_finding_pi.py
+++ multi_finding_pi.py
@@ -2,18 +2,18 @@ from multiprocessing import Pool, cpu_count
 from random import random
 
 
-def find_pi(count_point: int) -> float:
+def find_pi(count_point: int) -> int:
     point_in_round = sum([1 for i in range(count_point)
                           if random()**2 + random()**2 <= 1])
-
-    return 4 * point_in_round / count_point
+    return point_in_round
 
 
 def main():
+    n = 100_000_000
+    part_count = [n//cpu_count() for i in range(cpu_count())]
     with Pool(processes=cpu_count()) as pool:
-        values = (5, 15, 20, 100, 1000, 10000, 10000000)
-        data = pool.map(find_pi, values)
-        print(data)
+        data = pool.map(find_pi, part_count)
+        print(4 * sum(data) / n)
 
 
 if __name__ == '__main__':
