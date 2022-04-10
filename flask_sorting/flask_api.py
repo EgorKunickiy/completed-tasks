f25012a633abacee6f7b7e71fa6545a912060503
EgorKunickiy <91185203+EgorKunickiy@users.noreply.github.com>
2022-04-11 01:31:30 +0300

    New sol

--- flask_sorting/flask_api.py
+++ flask_sorting/flask_api.py
@@ -1,57 +1,13 @@
 from flask import Flask, jsonify, request
-import requests
-
+import math_logic
 app = Flask(__name__)
 
-json = ''
 
-
-def calculate(json):
-    jsn_out = {
-        'SumResult': 0,
-        'MulResult': 1,
-        'SortedInputs': []
-    }
-    k = json['K']
-    sum = 0
-    for el in json['Sums']:
-        sum += el
-        jsn_out['SortedInputs'].append(el)
-
-    jsn_out['SumResult'] = round(sum * k, 2)
-
-    for el in json['Muls']:
-        jsn_out['MulResult'] *= el
-        jsn_out['SortedInputs'].append(el)
-
-    jsn_out['SortedInputs'].sort()
-    return jsn_out
-
-
-@app.route('/')
-def index():
-    return ''
-
-
-@app.route('/Ping')
-def ping():
-    res = requests.get('http://127.0.0.1:5000/')
-    if res.status_code == 200:
-        return jsonify(res.status_code)
-    else:
-        return 'server X_x'
-
-
-@app.route('/PostInputData', methods=['POST'])
+@app.route('/', methods=['POST'])
 def post_input_data():
-    jsonr = request.get_json()
-    print(jsonr)
-    return jsonify(jsonr)
-
-
-@app.route('/GetAnswer')
-def get_answer():
-    return jsonify(calculate(json))
+    f_json = request.get_json()
+    print(f_json)
+    return jsonify(f_json)
 
 
 if __name__ == '__main__':
