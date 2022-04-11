a6bde17b0ff33ee07f0ab21188836296a4603607
EgorKunickiy <91185203+EgorKunickiy@users.noreply.github.com>
2022-04-11 03:13:45 +0300

    New sol

--- flask_sorting/flask_api.py
+++ flask_sorting/flask_api.py
@@ -1,13 +1,35 @@
 from flask import Flask, jsonify, request
-import math_logic
+from math_logic import get_sort
+
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST'])
-def post_input_data():
+@app.route('/SelectionSort', methods=['POST'])
+def proc_selection_sort():
     f_json = request.get_json()
-    print(f_json)
-    return jsonify(f_json)
+    result = get_sort(f_json, "selection sort")
+    return jsonify(result)
+
+
+@app.route('/InsertionSort', methods=['POST'])
+def proc_insertion_sort():
+    f_json = request.get_json()
+    result = get_sort(f_json, "insertion sort")
+    return jsonify(result)
+
+
+@app.route('/HeapSort', methods=['POST'])
+def proc_heap_sort():
+    f_json = request.get_json()
+    result = get_sort(f_json, "heap sort")
+    return jsonify(result)
+
+
+@app.route('/FastSort', methods=['POST'])
+def proc_fast_sort():
+    f_json = request.get_json()
+    result = get_sort(f_json, "fast sort")
+    return jsonify(result)
 
 
 if __name__ == '__main__':
--- flask_sorting/math_logic.py
+++ flask_sorting/math_logic.py
@@ -1,1 +1,22 @@
-import flask_sorting
+from sorting_methods import insertion_sort, selection_sort, heap_sort, fast_sort
+from timeit import default_timer as timer
+from parser import parse
+
+LIST_OF_SORTING = {
+    "selection sort": selection_sort,
+    "insertion sort": insertion_sort,
+    "fast sort": fast_sort,
+    "heap sort": heap_sort
+}
+
+
+def get_sort(data: dict, func: str) -> dict:
+    final_dict = dict()
+    list_seq = parse(data)
+    for sequence in list_seq:
+        start = timer()
+        result = LIST_OF_SORTING[func](sequence)
+        end = timer()
+        final_dict[str(end-start)] = result
+
+    return final_dict
